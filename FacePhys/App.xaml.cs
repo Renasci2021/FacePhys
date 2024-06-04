fa4754fc38256d2bdb07f47730e4da6f9f5bdb65
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2024-06-04 11:06:59 +0800

    first commit

---
+++ FacePhys/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FacePhys
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FacePhys/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FacePhys
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
