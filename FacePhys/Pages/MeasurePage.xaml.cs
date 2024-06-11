94638aa751ea2901d5c70253bd6262309b7a9b72
chenzzz851 <1980103789@qq.com>
2024-06-11 17:49:52 +0800

    ui: 优化tablayout以及各个界面的ui

--- FacePhys/Pages/MeasurePage.xaml.cs
+++ FacePhys/Pages/MeasurePage.xaml.cs
@@ -120,11 +120,70 @@ public partial class MeasurePage : ContentPage
 	{
 		var surface = e.Surface;
 		var canvas = surface.Canvas;
+
 		canvas.Clear();
-		if (_skBitmap != null)
+
+		// if (_skBitmap != null)
+		// {
+		// 	canvas.DrawBitmap(_skBitmap, e.Info.Rect);
+		// }
+
+
+		// 获取画布的尺寸
+		var width = e.Info.Width-40;
+		var height = e.Info.Height-40;
+		var radius = Math.Min(width, height) / 2 -25;
+		var centerX = width / 2 + 20;
+		var centerY = height / 2 + 20;
+
+		// 清除画布
+		canvas.Clear(SKColors.Transparent);
+
+		// 绘制灰色的矩形，作为背景
+		var paint = new SKPaint
 		{
-			canvas.DrawBitmap(_skBitmap, e.Info.Rect);
+			Style = SKPaintStyle.Fill,
+			Color = SKColor.Parse("#F5F5F5"),
+			IsAntialias = true
+		};
+		canvas.DrawRect(0, 0, width+40, height+40, paint);
+
+		// 绘制绿色的圆周，不填充
+		paint.Style = SKPaintStyle.Stroke;
+		paint.Color = SKColor.Parse("#7CC587");
+		paint.StrokeWidth = 15; // 设置线条宽度
+		radius = radius + 30;
+		canvas.DrawCircle(centerX, centerY, radius, paint);
+		radius = radius- 30;
+		
+		// 创建一个圆形路径
+		using (var path = new SKPath())
+		{
+			path.AddCircle(centerX, centerY, radius);
+
+			// 裁剪画布以保留圆形区域
+			canvas.ClipPath(path);
+
+			// 如果 _skBitmap 存在，在圆形区域内绘制 _skBitmap
+			if (_skBitmap != null)
+			{
+				// 计算缩放因子和目标矩形
+				float scale = Math.Min((float)radius * 2 / _skBitmap.Width, (float)radius * 2 / _skBitmap.Height);
+				float bitmapWidth = scale * _skBitmap.Width;
+				float bitmapHeight = scale * _skBitmap.Height;
+				var destRect = new SKRect(
+					centerX - bitmapWidth / 2,
+					centerY - bitmapHeight / 2,
+					centerX + bitmapWidth / 2,
+					centerY + bitmapHeight / 2
+				);
+
+				// 绘制位图
+				canvas.DrawBitmap(_skBitmap, destRect);
+			}
 		}
+
+		canvas.Restore();
 	}
 
 	private async void OnDetectFaceClicked(object sender, EventArgs e)
